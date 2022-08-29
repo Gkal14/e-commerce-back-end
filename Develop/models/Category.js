9dd13f13ca85aa1948500a857c39780e1982260f
George Kaluwin <georgekaluwin@gmail.com>
2022-08-29 13:22:09 +0930

    models defined

--- Develop/models/Category.js
+++ Develop/models/Category.js
@@ -7,6 +7,40 @@ class Category extends Model {}
 Category.init(
   {
     // define columns
+
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+
+    },
+    category_name: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    price:{
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        isDecimal: true
+      }
+    },
+    stock: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      defaultValue: 10,
+      validate: {
+        isNumeric: true
+      }
+    },
+    category_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'category',
+        key: 'id'
+      }
+    }
   },
   {
     sequelize,
